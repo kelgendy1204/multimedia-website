0fbc9a0ef9a53273c4a04b806a582890bb861262
Fares <mado.mazika2day@gmail.com>
2017-05-29 15:17:17 +0200

    add google analytics

--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -16,7 +16,15 @@
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/manifest.json">
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-        <link rel="stylesheet" href="/dist/css/home.css">
+        <link rel="stylesheet" href="/dist/css/home.css" />
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-6197253-1', 'auto');
+          ga('send', 'pageview');
+        </script>
     </head>
     <body class="page links">
         <div class="insidepage">
--- resources/views/posts/posts.blade.php
+++ resources/views/posts/posts.blade.php
@@ -36,9 +36,15 @@
         <link rel="manifest" href="/manifest.json">
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
         <link rel="canonical" href="{{Request::url()}}" />
-
-
         <link rel="stylesheet" href="/dist/css/home.css" />
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+          ga('create', 'UA-6197253-1', 'auto');
+          ga('send', 'pageview');
+        </script>
     </head>
     <body>
         <div class="mainpage">

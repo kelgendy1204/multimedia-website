4e845eb511881e1634d4521a5af43805fca5ed50
Khaled Saad <elgendy1204@gmail.com>
2017-06-02 16:23:01 +0200

    finish description and comments

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -19,7 +19,11 @@
 		toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | preview media fullpage | forecolor backcolor emoticons codesample fontsizeselect fontselect visualblocks',
 		imagetools_toolbar: 'rotateleft rotateright | flipv fliph | editimage imageoptions',
 		font_formats: 'Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats;JF=jf',
-		content_css : '/dist_v2/css/tinymce.css'
+		content_css : '/dist_v2/css/tinymce.css',
+		style_formats: [
+			{title: 'Description title', inline : 'span' ,classes: 'desctitle'}
+		],
+		style_formats_merge: true
 	});
 </script>
 
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.showpost')
 
 @section('content')
-	<div class="img"><img src="{{ $post->photo_url }}"></div>
-	<div class="post_description">
-		{!! $post->long_description !!}
+	<div class="postdetails">
+		<div class="post_description">
+			<div class="descriptioncontent">
+				<div class="long_description">
+					{!! $post->long_description !!}
+				</div>
+			</div>
+		</div>
+		<div class="img" style="background-image: url({{ $post->photo_url }})"></div>
+	</div>
+	<div class="share-title firstpage">
+		<div class="share">
+			<a class="socitem google-plus"></a>
+			<a class="socitem twitter"></a>
+			<a class="socitem facebook"></a>
+		</div>
 	</div>
 	<div class="download">
 		@if ($subpost)
@@ -15,4 +28,36 @@
 			<div class="left"><a href="{{route('download', ['postdesc' => $post->description])}}"><img src="/dist_v2/images/second/02.svg" alt="downloads" /></a></div>
 		@endif
 	</div>
+
+	<script>
+		(function () {
+			var serverLinks = document.querySelectorAll('.servers >a');
+			var iframe = document.querySelector('iframe');
+			serverLinks.forEach(function (item) {
+				item.addEventListener('click', function(e){
+					e.preventDefault();
+					document.querySelector('.servers >a.active').classList.remove('active');
+					iframe.src = this.href;
+					this.classList.add('active');
+				});
+			});
+
+			document.querySelector('.socitem.facebook').onclick = function() {
+				window.open("https://www.facebook.com/sharer/sharer.php?u={{Request::fullUrl()}}", "pop", "width=600, height=400, scrollbars=no");
+				return false;
+			};
+
+			document.querySelector('.socitem.google-plus').onclick = function() {
+				window.open('https://plus.google.com/share?url={{Request::fullUrl()}}', '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+				return false;
+			};
+
+			document.querySelector('.socitem.twitter').onclick = function() {
+				window.open('https://twitter.com/share?url={{Request::fullUrl()}}', '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');
+				return false;
+			};
+
+
+		}());
+	</script>
 @endsection

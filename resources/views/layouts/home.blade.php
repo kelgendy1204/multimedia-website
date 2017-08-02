d33ffd855ae85ef8114088bdceb87d5398890b6c
Khaled Saad <elgendy1204@gmail.com>
2017-08-02 22:29:56 +0200

    fix style in v7

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -67,7 +67,7 @@
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <link rel="canonical" href="{{ Request::fullUrl() }}" />
 
-        <link rel="stylesheet" href="/dist_v6/css/home-481c1bf8b4.css" />
+        <link rel="stylesheet" href="/dist_v6/css/home-bb71c2b818.css" />
         <script src="/dist_v6/uncompiled/jquery-3.2.1.min.js"></script>
         <script src="/dist_v6/uncompiled/jquery.sticky.js"></script>
 
--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -27,7 +27,7 @@
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/manifest.json">
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
-        <link rel="stylesheet" href="/dist_v6/css/home-481c1bf8b4.css" />
+        <link rel="stylesheet" href="/dist_v6/css/home-bb71c2b818.css" />
 
         @if ($scripts->value)
           {!! $scripts->value !!}
